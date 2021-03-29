dcf216f2c01e74ce25119a46f3cf4ca8b98d6af7
Benjamin Ju <fapply1222@gmail.com>
2021-03-30 02:37:06 +0900

    30/March

---
+++ index.ts
@@ -0,0 +1,10 @@
+const name = "benjamin",
+age = 24,
+gender = "male";
+
+const sayHi  = (name,age,gender?) =>{
+    console.log(`hello ${name}, you are ${age}, you're a ${gender}`);
+};
+
+sayHi(name,age);
+export {};
